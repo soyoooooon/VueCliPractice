257f8633514a07d4af09ff8cf4b09d5a092a6dcc
이소윤 <s_lee77176@fanshaweonline.ca>
2020-07-31 17:21:15 +0900

    practicing library

--- vue-practice/babel.config.js
+++ vue-practice/babel.config.js
@@ -3,3 +3,6 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+
+
+//converting any other code that older virson
--- vue-practice/src/main.js
+++ vue-practice/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'animate.css'
+import 'fullpage-vue/src/fullpage.css'
+import VueFullpage from 'fullpage-vue'
+Vue.use(VueFullpage)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+//npm run serve
